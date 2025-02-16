ebfdbb997504f1c27b61631ba7ab2ad25b2edbe8
MirnaAna <mirnabindang@gmail.com>
2025-02-16 11:03:01 +0100

    añadiendo la estructura del proyecto

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome(props) {
+    return (
+        <>
+            <Head title="Welcome" />
+        <div>Mirna en React</div>
+        </>
+    );
+}
