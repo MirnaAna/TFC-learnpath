a46cebdcc2cc7bd45c898ed9a3044beb16a2279d
MirnaAna <mirnabindang@gmail.com>
2025-04-01 14:33:29 +0200

    Inicio y registro actualizado

--- resources/js/Pages/Inicio.jsx
+++ resources/js/Pages/Inicio.jsx
@@ -4,13 +4,11 @@ import { Inertia } from "@inertiajs/inertia";
 import Header from "../Components/Header";
 
 export default function Inicio(props) {
-    const [formacionSeleccionada, setFormacionSeleccionada] = useState(
-        "Selecciona tu formación"
-    );
+    const [formacionSeleccionada, setFormacionSeleccionada] = useState("");
 
-    function gestionarRedireccion() {
-        console.log("hoaaaaaaaaaaa");
-        if (formacionSeleccionada) {
+    function gestionarRedireccionFormacion() {
+        console.log("hoaaaaaaaaaaa", typeof formacionSeleccionada);
+        if (formacionSeleccionada != "") {
             Inertia.visit(
                 `/detalles-formacion/${formacionSeleccionada.toLocaleUpperCase()}`
             );
@@ -36,10 +34,9 @@ export default function Inicio(props) {
                             setFormacionSeleccionada(e.target.value)
                         }
                     >
-                        <option
-                            disabled
-                            value="Selecciona tu formación"
-                        ></option>
+                        <option value="" disabled hidden selected>
+                            --selecciona una formacion--
+                        </option>
                         <option value="ASIR">ASIR</option>
                         <option value="DAM">DAM</option>
                         <option value="DAW">DAW</option>
@@ -47,10 +44,21 @@ export default function Inicio(props) {
                     {/*añadims un boton*/}
                     <button
                         className=" border-2 bg-purple-500 hover:bg-blue-700 text-white font-semibold py-2 px-4"
-                        onClick={gestionarRedireccion}
+                        onClick={gestionarRedireccionFormacion}
                     >
                         Ok
                     </button>
+                    <button className="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150 ">
+                        iniciar sesion
+                    </button>
+                    <div className="mt-10">
+                        <Link
+                            href="/register"
+                            className="bg-yellow-400 text-gray-900 px-6 py-3 rounded-xl shadow-lg hover:bg-yellow-500 hover:scale-105 transition-transform font-semibold text-lg"
+                        >
+                            registrar
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
